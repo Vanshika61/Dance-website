905fd96b3c62f47a598ba20ff27f67c5b7ba2eb0
Vanshika <agrawal10614@gmail.com>
2023-06-18 18:11:46 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const express = require('express');
 const path = require("path");
-const fs = require("fs");
 const app = express();
-const port = 80;
+const port = 8000;
+
+
+// EXPRESS SPECIFIC CONFIGURATION 
+app.use('/static', express.static('static'));   // For serving static files
+app.use(express.urlencoded());
+
+// PUG SPECIFIC CONFIGURATION 
+app.set('view engine', 'pug');      // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views'));        // Set the views directory
+
+
+
+// ENDPOINTS CONFIGURATION
+app.get('/', (req, res) => {
+    const params = {}
+    res.status(200).render('index', params);
+});
+
+
+
+
+// START THE SERVER
+app.listen(port, () =>{
+    console.log(`The application started successfully on port ${port}`)
+});
